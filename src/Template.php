bee1ea7fc975daf2c90f866ba8bfdb3e2fbb41af
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-28 03:03:16 +0000

    First commit.

---
+++ src/Template.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ocdla;
+
+
+
+	
+class Template {
+
+
+	public static function renderTemplate($path, $vars = array()) {
+
+		extract($vars);
+
+		ob_start();
+
+		require $path;
+		$content = ob_get_contents(); // get contents of buffer
+		ob_end_clean();
+
+		return $content;
+	}
+
+
+
+}
