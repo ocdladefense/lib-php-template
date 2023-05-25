b989634a5bcdac3c7e4994c1ed743ac21c4c87a3
Jose Bernal <jbernal.web.dev@gmail.com>
2023-05-25 01:45:55 +0000

    Refactor render function.

--- src/Template.php
+++ src/Template.php
@@ -7,6 +7,26 @@ namespace Ocdla;
 	
 class Template {
 
+	private $path;
+
+
+	public function __construct($path) {
+		$this->path = $path;
+	}
+
+	
+	public function render($vars) {
+		extract($vars);
+
+		ob_start();
+
+		require $this->path;
+		$content = ob_get_contents(); // get contents of buffer
+		ob_end_clean();
+
+		return $content;
+	}
+
 
 	public static function renderTemplate($path, $vars = array()) {
 
@@ -19,11 +39,6 @@ class Template {
 		ob_end_clean();
 
 		return $content;
-	}
-
-	
-	public static function render($path, $vars = array()) {
-		return self::renderTemplate($path, $vars);
 	}
 
 
