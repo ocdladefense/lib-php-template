c6da65492f551352c575627b4700b5af3b78ec1e
Jose Bernal <jbernal.web.dev@gmail.com>
2023-06-05 08:25:11 +0000

    Update.

--- src/Template.php
+++ src/Template.php
@@ -10,12 +10,39 @@ class Template {
 	private $path;
 
 
+	private $name;
+
+
+	private $hasPreprocessor;
+
+	
+
 	public function __construct($path) {
-		$this->path = $path;
+		$this->path = $path . ".tpl.php";
+
+		$parts = explode(DIRECTORY_SEPARATOR, $path);
+
+		$this->name = array_pop($parts);
+
+		$path_only = implode(DIRECTORY_SEPARATOR, $parts);
+
+		$file = $path_only . DIRECTORY_SEPARATOR . "preprocess.php";
+
+		$this->hasPreprocessor = @include $file;
 	}
+
+
 
 	
 	public function render($vars) {
+
+		$preprocess = $this->name . "_preprocess";
+
+
+		if(function_exists($preprocess)) {
+			$vars = call_user_func($preprocess, $vars);
+		}
+
 		extract($vars);
 
 		ob_start();
